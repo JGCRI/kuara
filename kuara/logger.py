8f93f9db4f9e99a93269e082fb1eb4f81c0b7bfe
crvernon <chrisrvernon@gmail.com>
2021-06-22 14:58:27 -0400

    set logger to console handler only

--- kuara/logger.py
+++ kuara/logger.py
@@ -8,14 +8,13 @@ class Logger:
     # output format for log string
     LOG_FORMAT_STRING = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
-    @property
-    def log_format(self):
-        """Generate log formatter."""
+    def __init__(self):
 
-        return logging.Formatter(self.LOG_FORMAT_STRING)
+        self.log_format = logging.Formatter(self.LOG_FORMAT_STRING)
+        self.logger = self.set_logger()
+        self.initialize_logger()
 
-    @property
-    def logger(self):
+    def set_logger(self):
         """Initialize logger as level info."""
 
         logger = logging.getLogger()
@@ -24,30 +23,12 @@ class Logger:
         return logger
 
     def initialize_logger(self):
-        """Initialize logger to stdout and file."""
-
-        # logger console handler
-        self.console_handler()
-
-        # logger file handler
-        if self.write_logfile:
-            self.file_handler()
-
-    def console_handler(self):
-        """Construct console handler."""
+        """Construct console handler and initialize logger to stdout."""
 
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setLevel(logging.INFO)
         console_handler.setFormatter(self.log_format)
         self.logger.addHandler(console_handler)
-
-    def file_handler(self):
-        """Construct file handler."""
-
-        file_handler = logging.FileHandler(self.logfile)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(self.log_format)
-        self.logger.addHandler(file_handler)
 
     @staticmethod
     def close_logger():
