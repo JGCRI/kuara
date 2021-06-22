86b5e71d21a933a9fab80657a5e99ac6c10871de
crvernon <chrisrvernon@gmail.com>
2021-06-22 14:56:16 -0400

    raise climate reader fn to pkg level

--- kuara/__init__.py
+++ kuara/__init__.py
@@ -1,3 +1,5 @@
-from kuara.technical_potential import process_climate, get_hours_per_year, calc_total_suitable_area, calc_technical_potential
+from kuara.technical_potential import get_hours_per_year, calc_total_suitable_area, calc_technical_potential
+from kuara.climate import process_climate_data
 
-__all__ = ['process_climate', 'get_hours_per_year', 'calc_total_suitable_area', 'calc_technical_potential']
+
+__all__ = ['process_climate_data', 'get_hours_per_year', 'calc_total_suitable_area', 'calc_technical_potential']
